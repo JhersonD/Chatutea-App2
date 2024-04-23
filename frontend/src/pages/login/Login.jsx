d15b37d56ef8bb18f6eb06034cfdad930d6c1b54
Jherson Chavez <jhersondannychavezmontes@gmail.com>
2024-04-22 21:46:24 -0500

    Modificacion de UI

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -17,18 +17,18 @@ const Login = () => {
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Login
-					<span className='text-blue-500'> ChatApp</span>
+					Iniciar Sesion
+					<span className='text-blue-500'> Chatutea App</span>
 				</h1>
 
 				<form onSubmit={handleSubmit}>
 					<div>
 						<label className='label p-2'>
-							<span className='text-base label-text'>Username</span>
+							<span className='text-base label-text'>Nombre de Usuario</span>
 						</label>
 						<input
 							type='text'
-							placeholder='Enter username'
+							placeholder='Ingrese Nombre de Usuario'
 							className='w-full input input-bordered h-10'
 							value={username}
 							onChange={(e) => setUsername(e.target.value)}
@@ -37,18 +37,18 @@ const Login = () => {
 
 					<div>
 						<label className='label'>
-							<span className='text-base label-text'>Password</span>
+							<span className='text-base label-text'>Contraseña</span>
 						</label>
 						<input
 							type='password'
-							placeholder='Enter Password'
+							placeholder='Ingrese su Contraseña'
 							className='w-full input input-bordered h-10'
 							value={password}
 							onChange={(e) => setPassword(e.target.value)}
 						/>
 					</div>
 					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
-						{"Don't"} have an account?
+						{"¿No"} tiene una cuenta?
 					</Link>
 
 					<div>
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -27,13 +27,13 @@ const SignUp = () => {
 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Sign Up <span className='text-blue-500'> ChatApp</span>
+					Registrarse <span className='text-blue-500'> Chatutea App</span>
 				</h1>
 
 				<form onSubmit={handleSubmit}>
 					<div>
 						<label className='label p-2'>
-							<span className='text-base label-text'>Full Name</span>
+							<span className='text-base label-text'>Nombre Completo</span>
 						</label>
 						<input
 							type='text'
@@ -46,7 +46,7 @@ const SignUp = () => {
 
 					<div>
 						<label className='label p-2 '>
-							<span className='text-base label-text'>Username</span>
+							<span className='text-base label-text'>Nombre de Usuario</span>
 						</label>
 						<input
 							type='text'
@@ -59,11 +59,11 @@ const SignUp = () => {
 
 					<div>
 						<label className='label'>
-							<span className='text-base label-text'>Password</span>
+							<span className='text-base label-text'>Contraseña</span>
 						</label>
 						<input
 							type='password'
-							placeholder='Enter Password'
+							placeholder='Ingrese una contraseña'
 							className='w-full input input-bordered h-10'
 							value={inputs.password}
 							onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
@@ -72,11 +72,11 @@ const SignUp = () => {
 
 					<div>
 						<label className='label'>
-							<span className='text-base label-text'>Confirm Password</span>
+							<span className='text-base label-text'>Confirmar Contraseña</span>
 						</label>
 						<input
 							type='password'
-							placeholder='Confirm Password'
+							placeholder='Confirmar Contraseña'
 							className='w-full input input-bordered h-10'
 							value={inputs.confirmPassword}
 							onChange={(e) => setInputs({ ...inputs, confirmPassword: e.target.value })}
@@ -90,7 +90,7 @@ const SignUp = () => {
 						className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'
 						href='#'
 					>
-						Already have an account?
+						¿Ya tiene una cuenta?
 					</Link>
 
 					<div>
